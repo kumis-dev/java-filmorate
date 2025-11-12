e5f37e5d6d1e78b92aa784b4e68aa583d175b77e
Ivan <kumis55555@yandex.ru>
2025-11-12 22:23:45 +0300

    Поменять исключение

--- src/main/java/ru/yandex/practicum/filmorate/exceptions/UpdateTargetNotFoundValidationException.java
+++ src/main/java/ru/yandex/practicum/filmorate/exceptions/UpdateTargetNotFoundValidationException.java
@@ -1,6 +1,6 @@
 package ru.yandex.practicum.filmorate.exceptions;
 
-public class UpdateTargetNotFoundValidationException extends ValidationException {
+public class UpdateTargetNotFoundValidationException extends RuntimeException {
     public UpdateTargetNotFoundValidationException(String message) {
         super(message);
     }
