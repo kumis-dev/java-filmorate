68997a6f61fd4fb8edd15b73711ed976b4de2b72
Ivan <kumis55555@yandex.ru>
2025-11-12 22:16:39 +0300

    Поменять обработчик ошибки местами

--- src/main/java/ru/yandex/practicum/filmorate/handlers/ErrorHandler.java
+++ src/main/java/ru/yandex/practicum/filmorate/handlers/ErrorHandler.java
@@ -12,29 +12,32 @@ import ru.yandex.practicum.filmorate.exceptions.ValidationException;
 @RestControllerAdvice
 @Slf4j
 public class ErrorHandler {
+
+    // Самые специфичные исключения - ПЕРВЫМИ
+    @ExceptionHandler(UpdateTargetNotFoundValidationException.class)
+    @ResponseStatus(HttpStatus.NOT_FOUND) // 404
+    public ErrorResponse handleUpdateTargetMissing(UpdateTargetNotFoundValidationException e) {
+        return new ErrorResponse(e.getMessage());
+    }
+
+    @ExceptionHandler(NotFoundException.class)
+    @ResponseStatus(HttpStatus.NOT_FOUND) // 404
+    public ErrorResponse handleNotFound(NotFoundException e) {
+        return new ErrorResponse("Объект не найден");
+    }
+
     @ExceptionHandler(ValidationException.class)
     @ResponseStatus(HttpStatus.BAD_REQUEST) // 400
     public ErrorResponse handleValidation(ValidationException e) {
         return new ErrorResponse(e.getMessage());
     }
 
-    @ExceptionHandler(NotFoundException.class)
-    @ResponseStatus(HttpStatus.NOT_FOUND) // 404
-    public ErrorResponse handleNotFound(RuntimeException e) {
-        return new ErrorResponse("Объект не найден");
-    }
-
+    // Самый общий обработчик - ПОСЛЕДНИМ
     @ExceptionHandler(Throwable.class)
     @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR) // 500
     public ErrorResponse handleThrowable(Throwable e) {
+        log.error("Внутренняя ошибка:", e);
         return new ErrorResponse("Внутренняя ошибка");
     }
-
-    @ExceptionHandler(UpdateTargetNotFoundValidationException.class)
-    @ResponseStatus(HttpStatus.NOT_FOUND) // 404 для апдейта несуществующего ресурса
-    public ErrorResponse handleUpdateTargetMissing(UpdateTargetNotFoundValidationException e) {
-        return new ErrorResponse(e.getMessage());
-    }
-
 }
 
